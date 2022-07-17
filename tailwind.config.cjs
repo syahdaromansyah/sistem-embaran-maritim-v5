99dcc1c4d352048639638489929a35e441f5490f
Syahda Romansyah <syahdarom@gmail.com>
2022-07-17 07:23:36 +0700

    chore: add Tailwind CSS screen breakpoints

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,11 @@ const twDefaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   content: ['./src/**/*.{html,ts,tsx}'],
   theme: {
+    screens: {
+      xs: '375px',
+      '2xs': '475px',
+      ...twDefaultTheme.screens,
+    },
     extend: {
       fontFamily: {
         inter: ['Inter', ...twDefaultTheme.fontFamily.sans],
