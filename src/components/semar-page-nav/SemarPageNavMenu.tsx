daf7f0524d1e22ab0e69e95cc895d8a7512e7761
Syahda Romansyah <syahdarom@gmail.com>
2022-07-17 10:59:49 +0700

    feat: create Semar page nav React components

---
+++ src/components/semar-page-nav/SemarPageNavMenu.tsx
@@ -0,0 +1,23 @@
+export default function SemarPageNavMenu() {
+  return (
+    <ul className="hidden md:flex md:items-center md:gap-4">
+      <li>
+        <a
+          href="/"
+          className="inline-block px-4 py-3 transition duration-300 hover:text-sky-400 focus:rounded-none focus:outline-none focus:ring focus:ring-sky-300 focus:ring-offset-2 focus:ring-offset-slate-50"
+        >
+          Masuk
+        </a>
+      </li>
+
+      <li>
+        <a
+          href="/"
+          className="inline-block px-4 py-3 transition duration-300 hover:text-sky-400 focus:rounded-none focus:outline-none focus:ring focus:ring-sky-300 focus:ring-offset-2 focus:ring-offset-slate-50"
+        >
+          Kontak Kami
+        </a>
+      </li>
+    </ul>
+  );
+}
