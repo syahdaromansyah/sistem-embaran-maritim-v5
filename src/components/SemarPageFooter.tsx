61d019f40a6cab10f3a3e9cb5ef2508e5e52bcca
Syahda Romansyah <syahdarom@gmail.com>
2022-07-25 14:41:10 +0700

    feat: update SemarPageFooter React component content

--- src/components/SemarPageFooter.tsx
+++ src/components/SemarPageFooter.tsx
@@ -59,20 +59,20 @@ export default function SemarPageFooter() {
             <section className="mb-10 md:mb-0 md:flex-1">
               <h3 className="mb-4 text-xl font-bold">Kontak</h3>
               <address className="not-italic">
-                <p>Lapan Pusat</p>
-                <p className="mb-2">Jl. Pemuda Persil No. 1</p>
+                <p>Badan Riset dan Inovasi Nasional</p>
+                <p className="mb-2">Jl. Dr. Djunjunan No.133</p>
                 <p>
-                  <strong>Jakarta Timur, POS 13220</strong>
+                  <strong>Bandung, POS 40173</strong>
                 </p>
 
                 <p>
-                  Telepon:
+                  Telepon:{' '}
                   <a
                     href="tel:+620214892802"
                     className="underline transition duration-300 focus:outline-none focus:ring focus:ring-sky-300 focus:ring-offset-2 focus:ring-offset-sky-100"
                   >
                     (021) 489-2802
-                  </a>
+                  </a>{' '}
                   Fax. 4894815
                 </p>
 
@@ -82,7 +82,7 @@ export default function SemarPageFooter() {
                     href="mailto:humas@lapan.go.id"
                     className="underline transition duration-300 focus:outline-none focus:ring focus:ring-sky-300 focus:ring-offset-2 focus:ring-offset-sky-100"
                   >
-                    humas@lapan.go.id
+                    prima@brin.go.id
                   </a>
                 </p>
               </address>
@@ -105,19 +105,21 @@ export default function SemarPageFooter() {
               </ul>
             </section>
 
-            {/* Footer LAPAN Profile */}
+            {/* Footer BRIN Profile */}
             <section className="mb-10 md:mb-0 md:flex-1">
-              <h3 className="mb-4 text-xl font-bold">Profil LAPAN</h3>
+              <h3 className="mb-4 text-xl font-bold">Profil BRIN</h3>
               <p>
-                Pada tanggal 31 Mei 1962, dibentuk Panitia Astronoutika oleh
-                Menteri Pertama RI, Ir. Juanda (selaku Ketua Dewan Penerbangan
-                RI) dan R.J. Salatun (selaku Sekretaris Dewan Penerbangan RI).
-                Tanggal 22 September 1962, terbentuknya proyek Roket.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Corrupti, adipisci explicabo soluta libero, cupiditate ducimus
+                unde excepturi porro ipsum dolorem voluptates placeat
+                reprehenderit ipsam fugit, harum minus dignissimos! Ut odio non
+                illo soluta aut nam amet deserunt suscipit! Eveniet sit
+                praesentium temporibus.
               </p>
             </section>
           </div>
 
-          <p className="text-center">&copy; LAPAN. Hak cipta dilindungi.</p>
+          <p className="text-center">&copy;BRIN. Hak cipta dilindungi.</p>
         </div>
       </div>
     </footer>
