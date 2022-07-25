066a7ade0a1a82356d521a1d8ac3cc35e2a091e1
Syahda Romansyah <syahdarom@gmail.com>
2022-07-25 14:34:08 +0700

    feat: revision Semar header description text

--- src/pages/home/components/main-sections/MainSection.tsx
+++ src/pages/home/components/main-sections/MainSection.tsx
@@ -20,11 +20,10 @@ export default function MainSection() {
                 dan peningkatan produksi perikanan tangkap. SEMAR merupakan
                 prototipe produk kerjasama pengembangan sistem pendukung
                 keputusan antara Pusat Sains dan Teknologi Atmosfer (PSTA),
-                Pusat Pemanfaatan Penginderaan Jauh (Pusfatja), Pusat Teknologi
-                Satelit (Pusteksat), dan Pusat Sains Antariksa (Pussainsa) LAPAN
-                dengan Dinas Kelautan dan Perikanan Propinsi Daerah Istimewa
-                Yogyakarta DKP (DIY), Kementerian Kelautan dan Perikanan (KKP),
-                dan Badan Meteorologi, Geofisika, dan Klimatologi (BMKG).
+                Pusat Riset Penginderaan Jauh BRIN, Pusat Riset Satelit BRIN,
+                Pusat Riset Antariksa BRIN, Pemerintah Provinsi DIY, Kementerian
+                Kelautan dan Perikanan (KKP), dan Badan Meteorologi, Geofisika,
+                dan Klimatologi (BMKG).
               </p>
             </header>
 
